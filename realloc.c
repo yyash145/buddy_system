a1167eb861241694148e26251a2a89b73d9c7aa8
SanyaGandhi <gandhisanya99@gmail.com>
2022-11-28 23:21:26 +0530

    realloc changed

--- realloc.c
+++ realloc.c
@@ -1,59 +1,97 @@
 #include "malloc.h"
 
-void *realloc(void *p, size_t size) {
-	size_t s;
-	m_block b, new;
+void *realloc(void *ptr, size_t size)
+{
 	void *newp = NULL;
-	if (!p || !arena) // if pointer is null just allocate the memory to current break
-		return (malloc(size));
-	if (is_valid_address (arena, p)) { // Reallocate only if its a valid address, as we need to free the old address
+	size_t s;
+	m_block b;
+	m_block new;
+	if (is_valid_address(arena, ptr))
+	{
+		b = get_block(ptr);
 		s = align8(size);
- 		b = get_block(p);
-		if (b->size >= s) {
-			if (mlock(b, s) == 0) {
-				if (b->buddy_order > get_buddy_order(s)) { // Same as malloc, split if chunk is bigger than required memory
-	 				split_block (arena, b, s);
+
+		if (b->size >= s)
+		{
+			if (b->next && b->next->free)
+			{
+				if ((b->size + m_block_size + b->next->size) >= s)
+				{
+					if (mlock(b, s) == 0)
+					{
+						b = buddy_join(arena, b);
+						if (b->buddy_order > get_buddy_order(s))
+						{
+							split_block(arena, b, s);
+						}
+						b->free = 0;
+						munlock(b, s);
+					}
+				}
+			}
+			else
+			{
+				newp = malloc(s); // allocate the memory
+				new = get_block(newp);
+				if (mlock(new, s) != 0)
+				{
+					printf("Failed to lock\n");
+					errno = ENOMEM; // Error if no more memory can be allocated
+					return (NULL);
+				}
+				else
+				{
+					copy_block(b, new); // copy data from previous to new block
+					munlock(new, s);
+				}
+				if (newp)
+				{
+					// don't do anything
+				}
+				else
+				{
+					printf("No block\n");
+					errno = ENOMEM;
+					return (NULL);
+				}
+				free(ptr); // free the old one
+				return (newp);
+			}
+		}
+		else
+		{
+
+			if (mlock(b, s) != 0)
+			{
+				printf("Failed to lock\n");
+				errno = ENOMEM; // Error if no more memory can be allocated
+				return (NULL);
+			}
+			else
+			{
+				if (b->buddy_order > get_buddy_order(s))
+				{ // Same as malloc, split if chunk is bigger than required memory
+					split_block(arena, b, s);
 				}
 				b->free = 0;
 				munlock(b, s);
-			} else {
-				printf("Failed to lock\n");
-				errno = ENOMEM; // Error if no more memory can be allocated
- 	 	 	 	return (NULL);
 			}
-		} else {
-			if (b->next && b->next->free && (b->size + m_block_size + b->next->size) >= s) { // try joining the next free chunk
-				if (mlock(b, s) == 0) {
-					b = buddy_join(arena, b);
-					if (b->buddy_order > get_buddy_order(s)) {
-						split_block (arena, b, s);
-					}
-					b->free = 0;
-					munlock(b, s);
-				}
-			} else {
-					newp = malloc(s); // allocate the memory
-					if (!newp) {
-						printf("No block\n");
-						errno = ENOMEM;
-			 			return (NULL);
-					}
-					new = get_block(newp);
-					if(mlock(new, s) == 0) {
-						copy_block(b, new); // copy data from previous to new block
-						munlock(new, s);
-					} else {
-						printf("Failed to lock\n");
-						errno = ENOMEM; // Error if no more memory can be allocated
-		 	 	 	 	return(NULL);
-					}
-					free(p); // free the old one
-					return (newp);
-				}
-    	}
- 			return (p); // return the old one
 		}
-		printf("Invalid pointer\n");
-		errno = ENOMEM;
-		return (NULL);
+		return (ptr); // return the old one
+	}
+	if (!ptr) // if pointer is null just allocate the memory to current break
+	{
+		int x=27;
+		size=x+size;
+		//subtracting..
+		size-=x;
+		return (malloc(size));
+	}
+	if (!arena)
+	{
+		return (malloc(size));
+	}
+	printf("Pointer not valid\n");
+	errno = ENOMEM;
+	return (NULL);
 }
